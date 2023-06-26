8879043efbcf2c5b9f2117f0185eb1a6ce69427c
Sowmiya-blueoshan <137379767+Sowmiya-blueoshan@users.noreply.github.com>
2023-06-26 17:42:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,101 +26,136 @@ let location1 = [
     }
 ]
 
-let job1 = {
-    Banglore: [{
+let job1 = [
+    {
         role: "Backend Engineer",
         code: "BEE",
+        city: "Banglore",
+        country: "India"
     },
     {
         role: "Business Analyst",
         code: "CRMBA",
+        city: "Banglore",
+        country: "India"
     },
     {
         role: "Technical Consultant",
         code: "MSSQL",
+        city: "Delhi",
+        country: "India"
     },
     {
         role: "Senior Developer",
         code: "REACTCON",
+        city: "Delhi",
+        country: "India"
     }
-    ],
+    ,
 
-    Chennai: [{
-        role: "Backend Engineer",
-        code: "BEE",
-    },
     {
         role: "Project Manager - Implementation",
         code: "PMHRPOOI",
+        city: "Chennai | Banglore | Mumbai",
+        country: "India"
     },
     {
         role: "Payroll Implementation Consultant",
         code: "HRPOOI",
+        city: "Chennai | Banglore | Mumbai",
+        country: "India"
     },
     {
         role: "Senior System Analyst (SQL)",
         code: "INDSQLDEV",
+        city: "Chennai",
+        country: "India"
     },
     {
         role: "Senior Business Analyst",
         code: "WMSFC",
+        city: "Chennai | Banglore | Mumbai",
+        country: "India"
     },
-    ],
 
-    Mumbai: [{
+
+    {
         role: "Payroll Processor",
         code: "BPOPP",
+        city: "Chennai | Banglore",
+        country: "India"
     },
     {
         role: "Service Delivery Manager - Payroll",
         code: "BPOSDM",
+        city: "Chennai | Banglore | Delhi",
+        country: "India"
     },
     {
         role: "Payroll Specialist",
         code: "BPOPS",
+        city: "Chennai | Banglore | Delhi",
+        country: "India"
     },
     {
         role: "Head- Customer Success",
         code: "AADHCS",
+        city: "Chennai | Banglore",
+        country: "India"
     }
-    ],
+    ,
 
-    Delhi: [{
+    {
         role: "Content Editor",
         code: "GMSCME",
+        city: "Mumbai",
+        country: "India"
     },
     {
         role: "Content Writer",
         code: "cw01",
+        city: "Mumbai",
+        country: "India"
     },
     {
         role: "FC",
         code: "AAD-Implementation Head",
+        city: "Chennai",
+        country: "India"
     },
     {
         role: "TC",
         code: "power Bl - ERP",
+        city: "Chennai",
+        country: "India"
     },
-    ],
 
-    Madurai: [{
+    {
         role: "TC",
         code: "Java and oracle",
+        city: "Madurai",
+        country: "India"
     },
     {
         role: "TC",
         code: "SQL",
+        city: "Madurai",
+        country: "India"
     },
     {
         role: "Partnership Operations Manager",
         code: "Marketing - Partnership operations Manager",
+        city: "Madurai",
+        country: "India"
     },
     {
         role: "TC",
         code: "React JS",
+        city: "Madurai",
+        country: "India"
     }
-    ]
-}
+
+]
 
 function scroll_to_bottom() {
     document.getElementById('details').scrollIntoView({
@@ -130,8 +165,8 @@ function scroll_to_bottom() {
 }
 
 function initMap() {
-    let loc;
-    let temp;
+    let count = 0;
+    let job2 = [];
     let center = { lat: 22.87139059648272, lng: 78.84618785140242 };
     let map = new google.maps.Map(document.getElementById('map'), {
         center: center,
@@ -159,60 +194,51 @@ function initMap() {
         let details = document.getElementById('details');
 
         marker.addListener('click', function () {
-
-            infoWindow.open(map, marker);
-            let place = Object.keys(job1);
             details.innerHTML = ' ';
-            console.log("[place]" + job1[place[0]][0].role);
+            count = 0;
+            infoWindow.open(map, marker);
 
-            for (let i = 0; i < place.length; i++) {
-
-                if (place[i] == marker.title) {
-                    loc = job1[place[i]];
-
-                    details.innerHTML += `<div class="col-12 d-flex justify-content-between pt-5">
-                                         <h5>Job Location : <span>${marker.title}</span></h5>
-                                         <h5>Open Positions : <span>${loc.length}</span></h5>
-                                     </div>`
-
-                    for (let i = 0; i < loc.length; i++) {
-                        temp = ' ';
-                        temp = marker.title;
-                        console.log("temp" + temp);
-                        for (let j = 0; j < place.length; j++) {
-
-                            if (place[j] != marker.title) {
-
-                                for (let k = 0; k < job1[place[j]].length; k++) {
-                                    console.log("loc[" + i + "].role is" + loc[i].role);
-                                    console.log("job1[place[" + j + "]][" + k + "].role is" + job1[place[j]][k].role);
-                                    if ((loc[i].role == job1[place[j]][k].role) && (loc[i].code == job1[place[j]][k].code)) {
-                                        console.log("gdshjds");
-                                        temp += ' | ' + place[j];
-                                        console.log("temp" + temp);
-                                    }
-                                }
-                            }
-                        }
-
-                        details.innerHTML += ` <div class="col-md-6 col-12 pt-4">
-                                               <div class="card shadow-lg border-0">
-                                                   <div class="card-body h-100 d-flex justify-content-between px-4 py-4">
-                                                       <div class="card-left">
-                                                           <h6 class="card-title">${loc[i].role}</h6>
-                                                           <p class="card-text">${temp}</p>
-                                                       </div>
-                                                       <div class="card-right">
-                                                           <h6 class="card-code">Job Code</h6>
-                                                           <p class="card-text">${loc[i].code}</p>
-                                                       </div>
-                                                   </div>
-                                                 </div>
-                                           </div>`
-                        console.log("234657");
+            let data = job1.filter((data) => {
+                let text = data.city.split('|');
+                console.log("text" + text);
+                for (let i = 0; i < text.length; i++) {
+                    console.log("text[" + i + "]" + text[i]);
+                    if (marker.title == (text[i])) {
+                        console.log("sds");
+                        count += 1;
+                        return data;
                     }
                 }
+            })
+            console.log(data);
+            console.log(count);
+
+            details.innerHTML += `<div class="col-12 d-flex justify-content-between pt-5">
+           <h5>Job Location : <span>${marker.title}</span></h5>
+           <h5>Open Positions : <span>${count}</span></h5>
+           </div>`
+
+            for (let i = 0; i < data.length; i++) {
+
+                details.innerHTML += ` <div class="col-md-6 col-12 pt-4">
+                                           <div class="card shadow-lg border-0">
+                                               <div class="card-body h-100 d-flex justify-content-between px-4 py-4">
+                                                   <div class="card-left">
+                                                       <h6 class="card-title">${data[i].role}</h6>
+                                                       <p class="card-text">${data[i].city}</p>
+                                                   </div>
+                                                   <div class="card-right">
+                                                       <h6 class="card-code">Job Code</h6>
+                                                       <p class="card-text">${data[i].code}</p>
+                                                   </div>
+                                               </div>
+                                             </div>
+                                       </div>`
+
             }
+
+
+
 
             setTimeout(scroll_to_bottom, 500);
 
@@ -224,6 +250,7 @@ function initMap() {
         });
     }
 }
+
 
 
 
